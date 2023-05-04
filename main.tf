1139d60400191143326c3aa0cb3e8eb03edc9fbc
smanikantavarma <smani327@gmail.com>
2023-05-04 21:18:52 +0530

    codeupdate

--- main.tf
+++ main.tf
@@ -15,15 +15,15 @@ terraform {
     storage_account_name = "devopsb27terraformprodu"
     container_name  = "tfstate"
     key = "devopsb27-git-class.tfstate"
-    access_key = "KFTC3rbIw7nkpnE6+ae+y9s4qv+nNMAJoDTj8bNPLFMvWVqAtq+x132ruaodWEBZdSJIRzLT43Ob+ASt/F7kCg=="
+  #  access_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  subscription_id = "df387313-b5f2-429f-8227-e142fc8c6b7b"
-  client_id = "429894e3-8edf-4fe8-b65f-02ab21610893"
-  client_secret = "DZN8Q~aPYMlbBaVMyF~EZeTblGshK-pfVUOz-ct_"
-  tenant_id = "ffab9f59-99fa-4165-96f0-2a6e4b9c46c5"
+# subscription_id = "aaaaaaa"
+# client_id = "bbbbbbbbb"
+# client_secret = "ccccccccc"
+# tenant_id = "ddddddddd"
   features {}
 }
