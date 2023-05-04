544c631ba7c669962abbe3aeda984dd8ea17b2b3
smanikantavarma <smani327@gmail.com>
2023-05-04 20:05:53 +0530

    code update

--- resource_group.tf
+++ resource_group.tf
@@ -12,3 +12,8 @@ resource "azurerm_resource_group" "rg3" {
     name = "devopsb27rg3"
     location = "eastus2"
 }
+
+resource "azurerm_resource_group" "rg4" {
+    name = "devopsb27rg4"
+    location = "eastus1"
+}
